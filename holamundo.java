816832e79c2ebf1d81fac64aedefc602112c0286
duartrufas <130761791+duartrufas@users.noreply.github.com>
2023-08-11 10:01:24 -0500

    Add files via upload

---
+++ holamundo.java
@@ -0,0 +1,16 @@
+
+
+ /* esto es para comentarios*/
+//clase principal de java
+ 
+
+/**
+ *
+ * @author Juan Carlos Bohorquez
+ */
+//Clase principal de java
+public class holamundo {
+    public static void main (String[] args){
+        System.out.println("Hola Juan");
+    }
+}
